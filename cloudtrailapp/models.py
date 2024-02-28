c4da905c7872900331e301ff25040c2495c4bb53
008oox <008oox@163.com>
2024-02-28 15:34:54 +0800

    cloudtrailRecord with page choice and Resouce of Event

--- cloudtrailapp/models.py
+++ cloudtrailapp/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class CloudTrailRecord(models.Model):
     UserName = models.CharField(max_length=30)
     EventName = models.CharField(max_length=50)
-    UserAgent = models.CharField(max_length=200, default="-")
-    EventType = models.CharField(max_length=30)
+    UserAgent = models.CharField(max_length=200)
+    ResourceName = models.CharField(max_length=200)
+    ResourceType = models.CharField(max_length=200)
     EventTime = models.DateTimeField()
     sourceIPAddr = models.GenericIPAddressField()
 
--- cloudtrailapp/views.py
+++ cloudtrailapp/views.py
@@ -15,6 +15,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .GetCloudTrail import getCloudTrail
 from django.http import JsonResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 
 
 def GetRecord():
@@ -24,6 +25,13 @@ def GetRecord():
     Start = getCloudTrail.Sync_time(End)
     UserList = getUserList(session)
     processed_events_list = []
+
+    def get_combined_resource_values(resources, key):
+        if resources:
+            return ", ".join(resource.get(key, "") for resource in resources)
+        else:
+            return "-"
+
     try:
         for Dict in UserList:
             User = Dict["UserName"]
@@ -35,9 +43,10 @@ def GetRecord():
                 output = {
                     "UserName": record["Username"],
                     "EventName": record["EventName"],
-                    "UserAgent": record["CloudTrailEvent"]["userAgent"][:100],
+                    "UserAgent": record["CloudTrailEvent"]["userAgent"][:200],
                     "EventTime": record["EventTime"],
-                    "EventType": record["CloudTrailEvent"]["eventType"],
+                    "ResourceType": get_combined_resource_values(record["Resources"], "ResourceType")[:200],
+                    "ResourceName": get_combined_resource_values(record["Resources"], "ResourceName")[:200],
                     "sourceIPAddr": record["CloudTrailEvent"]["sourceIPAddress"],
                 }
                 print(output)
@@ -78,27 +87,31 @@ def getUserList(session):
 def cloudtrail_records(request):
     username_filter = ""
     eventname_filter = ""
-    eventtype_filter = ""
+    resourcetype_filter = ""
+    resourcename_filter = ""
     sourceipaddr_filter = ""
     useragent_filter = ""
 
     if request.method == "POST":
         username_filter = request.POST.get("UserName", "")
         eventname_filter = request.POST.get("EventName", "")
-        eventtype_filter = request.POST.get("EventType", "")
+        resourcetype_filter = request.POST.get("ResourceType", "")
+        resourcename_filter = request.POST.get("ResourceName", "")
         sourceipaddr_filter = request.POST.get("sourceIPAddr", "")
         useragent_filter = request.POST.get("UserAgent", "")
 
         request.session["username_filter"] = username_filter
         request.session["eventname_filter"] = eventname_filter
-        request.session["eventtype_filter"] = eventtype_filter
+        request.session["resourcetype_filter"] = resourcetype_filter
+        request.session["resourcename_filter"] = resourcename_filter
         request.session["sourceipaddr_filter"] = sourceipaddr_filter
         request.session["useragent_filter"] = useragent_filter
 
     else:
         username_filter = request.session.get("username_filter", "")
         eventname_filter = request.session.get("eventname_filter", "")
-        eventtype_filter = request.session.get("eventtype_filter", "")
+        resourcetype_filter = request.session.get("resourcetype_filter", "")
+        resourcename_filter = request.session.get("resourcename_filter", "")
         sourceipaddr_filter = request.session.get("sourceipaddr_filter", "")
         useragent_filter = request.session.get("useragent_filter", "")
 
@@ -107,17 +120,19 @@ def cloudtrail_records(request):
         filters["UserName__icontains"] = username_filter
     if eventname_filter:
         filters["EventName__icontains"] = eventname_filter
-    if eventtype_filter:
-        filters["EventType__icontains"] = eventtype_filter
+    if resourcetype_filter:
+        filters["ResourceType__icontains"] = resourcetype_filter
+    if resourcename_filter:
+        filters["ResourceName__icontains"] = resourcename_filter
     if sourceipaddr_filter:
         filters["sourceIPAddr__icontains"] = sourceipaddr_filter
     if useragent_filter:
         filters["UserAgent__icontains"] = useragent_filter
 
-    records = CloudTrailRecord.objects.filter(**filters)
+    records = CloudTrailRecord.objects.filter(**filters).order_by("EventTime")
     items_per_page = 200
     paginator = Paginator(records, items_per_page)
-    page_number = request.GET.get("page", 1)
+    page_number = int(request.GET.get("page", 1))
 
     try:
         page_records = paginator.page(page_number)
@@ -133,7 +148,8 @@ def cloudtrail_records(request):
             "page_records": page_records,
             "username_filter": username_filter,
             "eventname_filter": eventname_filter,
-            "eventtype_filter": eventtype_filter,
+            "resourcetype_filter": resourcetype_filter,
+            "resourcename_filter": resourcename_filter,
             "sourceipaddr_filter": sourceipaddr_filter,
             "useragent_filter": useragent_filter,
         },
